c98a7506acc473894e8b53847d5b68550bb9570d
wahyu angga <wahyuangga@angz.ac.id>
2023-05-04 07:29:40 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode:'class',
+  theme: {
+    container:{
+      center : true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary:'#14b8a6',
+        jeruk: '#f97316',
+        secondary: '#64748b',
+        dark:'#020617',
+      },
+      screens :{
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
